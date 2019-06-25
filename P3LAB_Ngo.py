653ff6714914c6cae0750747d348d3709edfa02d
stevehngo <52019272+stevehngo@users.noreply.github.com>
2019-06-25 18:06:32 -0400

    Add files via upload
    p3lab

---
+++ P3LAB_Ngo.py
@@ -0,0 +1,39 @@
+# CTI - 110
+# P3LAB
+# Steve Ngo
+# 6-25-2019
+#
+
+def main():
+    # This program takes a number grade and outputs a letter grade.
+
+    # system uses 10-point grading scale
+    A_score = 90
+    B_score = 80
+    C_score = 70
+    D_score = 60
+    F_score = 60
+    # TO DO: define the rest of the scores
+
+    
+    score = int(input('Enter score: '))
+
+    if score >= A_score:
+        print('Your grade is: A')
+    else:
+        if score >= B_score:
+            print('Your grade is: B')
+        else:
+            if score >= C_score:
+                print('Your grade is: C')
+            else:
+                if score >= D_score:
+                    print('Your grade is: D')
+                else:
+                    print('Your grade is: F') # TO DO: finish this
+
+
+
+
+# program start
+main()
